1873f89235a96179b3bdb8382a5df01c5a801d7b
Kamil Szelągiewicz <kamil.szelagiewicz@gmail.com>
2025-07-08 18:22:22 +0200

    Refactor and fix bugs in Container class

--- src/main/java/pl/szelagi/state/Container.java
+++ src/main/java/pl/szelagi/state/Container.java
@@ -17,91 +17,62 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class Container<I, S> implements Serializable, Iterable<S> {
-    private final ManualContainer<I, S> manualContainer;
+public class Container<I, S> extends ManualContainer<I, S> {
     private final @NotNull Function<I, S> creator;
 
     public Container(@NotNull Function<I, S> creator) {
-        this.manualContainer = new ManualContainer<>();
         this.creator = creator;
     }
 
-    // CREATE
-
-    public @NotNull S createOrThrow(@NotNull I input, @NotNull Function<I, S> creator) throws ManualContainerException {
-        return manualContainer.createOrThrow(input, creator);
-    }
-
     public @NotNull S createOrThrow(@NotNull I input) throws ManualContainerException {
-        return manualContainer.createOrThrow(input, creator);
+        return createOrThrow(input, creator);
     }
-
-    // REFRESH
 
     public @NotNull S refreshOrCreate(@NotNull I input) {
         removeIfExists(input);
         return createOrThrow(input);
     }
 
-    public @NotNull S refreshOrThrow(@NotNull I input, @NotNull Function<I, S> creator) {
+    public @NotNull S refreshOrCreate(@NotNull I input, Function<I, S> creator) {
         removeIfExists(input);
         return createOrThrow(input, creator);
     }
 
-    // GET
-
-    public @NotNull S getOrCreate(I input) {
-        if (manualContainer.contains(input))
-            return manualContainer.getOrThrow(input);
-        return manualContainer.createOrThrow(input, creator);
+    public @NotNull S refreshOrThrow(@NotNull I input) {
+        removeOrThrow(input);
+        return createOrThrow(input);
     }
 
-    public @NotNull S getOrThrow(I input) throws ManualContainerException {
-        return manualContainer.getOrThrow(input);
+    public @NotNull S refreshOrThrow(@NotNull I input, @NotNull Function<I, S> creator) {
+        removeOrThrow(input);
+        return createOrThrow(input, creator);
+    }
+
+    public @NotNull S getOrCreate(I input) {
+        if (contains(input))
+            return getOrThrow(input);
+        return createOrThrow(input);
+    }
+
+    public @NotNull S getOrCreate(I input, @NotNull Function<I, S> creator) {
+        if (contains(input))
+            return getOrThrow(input);
+        return createOrThrow(input, creator);
     }
 
     public @Nullable S getOrNull(@NotNull I input) {
-        if (manualContainer.contains(input))
-            return manualContainer.getOrThrow(input);
+        if (contains(input))
+            return getOrThrow(input);
         return null;
-    }
-
-    // REMOVE
-
-    public @NotNull S removeOrThrow(@NotNull I input) throws ManualContainerException {
-        return manualContainer.removeOrThrow(input);
     }
 
     public @Nullable S removeIfExists(@NotNull I input) {
-        if (manualContainer.contains(input))
-            return manualContainer.removeOrThrow(input);
+        if (contains(input))
+            return removeOrThrow(input);
         return null;
     }
 
-    // EXISTS
-
-    public boolean contains(@NotNull I input) {
-        return manualContainer.contains(input);
-    }
-
-    // OPERATIONS
-
-    public @Nullable S find(Predicate<S> predicate) {
-        return manualContainer.find(predicate);
-    }
-
-    public <R> @NotNull List<R> map(Function<? super S, ? extends R> mapper) {
-        return manualContainer.map(mapper);
-    }
-
-    // GETTERS
-
     public @NotNull Function<I, S> creator() {
         return creator;
-    }
-
-    @Override
-    public @NotNull Iterator<S> iterator() {
-        return manualContainer.iterator();
     }
 }
