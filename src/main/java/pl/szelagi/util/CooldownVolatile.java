23649ab16024a19e69b35bb81775cb9b921d3e7e
Kamil Szelągiewicz <kamil.szelagiewicz@gmail.com>
2025-07-08 18:24:27 +0200

    Refactor CooldownVolatile to use NamespacedKey

--- src/main/java/pl/szelagi/util/CooldownVolatile.java
+++ src/main/java/pl/szelagi/util/CooldownVolatile.java
@@ -7,17 +7,19 @@
 
 package pl.szelagi.util;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.szelagi.util.timespigot.Time;
 import pl.szelagi.util.timespigot.TimeUnit;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CooldownVolatile {
     private static final int OPTIMIZE_TIMER_TICKS = 60 * 20 * 2;
     private static JavaPlugin plugin;
-    private static final HashMap<Player, HashMap<String, Long>> map = new HashMap<>();
+    private static final Map<Player, Map<NamespacedKey, Long>> map = new HashMap<>();
 
     public static void initialize(JavaPlugin p) {
         plugin = p;
@@ -25,54 +27,75 @@ public class CooldownVolatile {
                 .runTaskTimer(plugin, CooldownVolatile::optimize, OPTIMIZE_TIMER_TICKS, OPTIMIZE_TIMER_TICKS);
     }
 
-    public static void startCooldown(Player player, String name, Time span) {
-        HashMap<String, Long> playerCooldownMap;
-        playerCooldownMap = map.computeIfAbsent(player, k -> new HashMap<>());
-        long millisEnd = System.currentTimeMillis() + span.toMillis();
-        playerCooldownMap.put(name, millisEnd);
+    public static void startCooldown(Player player, NamespacedKey key, Time span) {
+        var playerCooldownMap = map.computeIfAbsent(player, k -> new HashMap<>());
+        var millisEnd = System.currentTimeMillis() + span.toMillis();
+        playerCooldownMap.put(key, millisEnd);
     }
 
-    public static boolean canUse(Player player, String name) {
+    @Deprecated
+    public static void startCooldown(Player player, String name, Time span) {
+        startCooldown(player, NamespacedKey.minecraft(name), span);
+    }
+
+    public static boolean canUse(Player player, NamespacedKey key) {
         var playerCooldownMap = map.get(player);
-        if (playerCooldownMap == null)
-            return true;
-        var cooldown = playerCooldownMap.get(name);
-        if (cooldown == null)
-            return true;
+        if (playerCooldownMap == null) return true;
+        var cooldown = playerCooldownMap.get(key);
+        if (cooldown == null) return true;
         return System.currentTimeMillis() >= cooldown;
     }
 
-    public static void deleteCooldown(Player player, String name) {
-        var playerCooldownMap = map.get(player);
-        if (playerCooldownMap == null)
-            return;
-        playerCooldownMap.remove(name);
+    @Deprecated
+    public static boolean canUse(Player player, String name) {
+        return canUse(player, NamespacedKey.minecraft(name));
     }
 
-    public static boolean canUseAndStart(Player player, String name, Time span) {
-        if (!canUse(player, name))
-            return false;
-        startCooldown(player, name, span);
+    public static void deleteCooldown(Player player, NamespacedKey key) {
+        var playerCooldownMap = map.get(player);
+        if (playerCooldownMap == null) return;
+        playerCooldownMap.remove(key);
+    }
+
+    public static void deleteCooldown(Player player, String name) {
+        deleteCooldown(player, NamespacedKey.minecraft(name));
+    }
+
+    public static boolean canUseAndStart(Player player, NamespacedKey key, Time span) {
+        if (!canUse(player, key)) return false;
+        startCooldown(player, key, span);
         return true;
     }
 
-    public static Time getTimeSpanToUse(Player player, String name) {
-        var dummyTimeSpan = new Time(0, TimeUnit.SECONDS);
+    @Deprecated
+    public static boolean canUseAndStart(Player player, String name, Time span) {
+        return canUseAndStart(player, NamespacedKey.minecraft(name), span);
+    }
+
+    public static Time getTimeSpanToUse(Player player, NamespacedKey key) {
         var playerCooldownMap = map.get(player);
         if (playerCooldownMap == null)
-            return dummyTimeSpan;
-        var millisEnd = playerCooldownMap.get(name);
+            return Time.zero();
+
+        var millisEnd = playerCooldownMap.get(key);
         if (millisEnd == null)
-            return dummyTimeSpan;
+            return Time.zero();
+
         var deltaMillis = millisEnd - System.currentTimeMillis();
         if (deltaMillis <= 0)
-            return dummyTimeSpan;
+            return Time.zero();
+
         return new Time(deltaMillis, TimeUnit.MILLIS);
+    }
+
+    @Deprecated
+    public static Time getTimeSpanToUse(Player player, String name) {
+        return getTimeSpanToUse(player, NamespacedKey.minecraft(name));
     }
 
     private static void optimize() {
         Player player;
-        HashMap<String, Long> playerCooldownMap;
+        Map<NamespacedKey, Long> playerCooldownMap;
         var currentMillis = System.currentTimeMillis();
         for (var entry : map.entrySet()) {
             player = entry.getKey();
