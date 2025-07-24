59de28cf57cdb846b9e2346152ef84771612598c
Kamil Szelągiewicz <kamil.szelagiewicz@gmail.com>
2025-07-24 05:11:23 +0200

    Add bot connection for end-to-end tests

--- src/test/java/e2e/CoreIT.java
+++ src/test/java/e2e/CoreIT.java
@@ -11,9 +11,11 @@ import com.github.t9t.minecraftrconclient.RconClient;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
 import org.testcontainers.shaded.org.awaitility.Awaitility;
 import org.testcontainers.utility.MountableFile;
 import pl.szelagi.command.test.IntegrationTestCommand;
@@ -28,6 +30,8 @@ import java.security.SecureRandom;
 import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Pattern;
+
 
 public class CoreIT {
     private static String generatePassword(int length) {
@@ -41,9 +45,11 @@ public class CoreIT {
 
     private static final String HOST = "localhost";
 
-
+    private static Network sharedNetwork;
     private static GenericContainer<?> server;
+    private static GenericContainer<?> bot;
     private static int port;
+    private static String host;
     private static String rconPassword;
     private static int rconPort;
     private static RconClient rcon;
@@ -79,6 +85,9 @@ public class CoreIT {
 
     @BeforeAll
     public static void setup() throws IOException, InterruptedException, URISyntaxException {
+        sharedNetwork = Network.newNetwork();
+
+
         rconPassword = generatePassword(16);
 
         var sapiPath = sapiPath();
@@ -86,7 +95,7 @@ public class CoreIT {
         var worldPath = worldPath();
 
         server = new GenericContainer<>("itzg/minecraft-server")
-                .withCreateContainerCmdModifier(cmd -> cmd.withName("sessionapi-test"))
+                .withCreateContainerCmdModifier(cmd -> cmd.withName("sessionapi-test-server"))
                 .withEnv("EULA", "TRUE")
                 .withEnv("TYPE", "PAPER")
                 .withEnv("VERSION", "1.21.1")
@@ -94,20 +103,23 @@ public class CoreIT {
                 .withEnv("RCON_PASSWORD", rconPassword)
                 .withEnv("RCON_PORT", "25575")
                 .withEnv("LEVEL_TYPE", "FLAT")
+                .withEnv("ONLINE_MODE", "FALSE")
+                .withEnv("DIFFICULTY", "peaceful")
                 .withExposedPorts(25565, 25575)
                 .withCopyFileToContainer(MountableFile.forHostPath(sapiPath.toString()), "/plugins/sapi.jar")
                 .withCopyFileToContainer(MountableFile.forHostPath(fawePath.toString()), "/plugins/fawe.jar")
-                .withCopyFileToContainer(MountableFile.forHostPath(worldPath.toString()), "/world.zip")
-                .withCommand("unzip -o /world.zip -d /world")
-                .waitingFor(org.testcontainers.containers.wait.strategy.Wait.forListeningPort());
+                .withNetwork(sharedNetwork)
+                .withNetworkAliases("mc-server");
+//                .withCopyFileToContainer(MountableFile.forHostPath(worldPath.toString()), "/world.zip")
+//                .withCommand("unzip -o /world.zip -d /world")
+//                .waitingFor(org.testcontainers.containers.wait.strategy.Wait.forListeningPort());
 
         System.out.println("Starting the container...");
         server.start();
         System.out.println("Waiting for the server to fully start...");
 
         Awaitility.await()
-                .atMost(Duration.ofMinutes(1))
-                .pollInterval(Duration.ofMillis(500))
+                .pollInterval(Duration.ofMillis(100))
                 .until(() -> {
                     String logs = server.getLogs();
                     return logs.contains("Done (");
@@ -115,26 +127,54 @@ public class CoreIT {
 
 
         port = server.getMappedPort(25565);
+        host = server.getHost();
         rconPort = server.getMappedPort(25575);
         System.out.println("Minecraft server started on port: " + port);
         System.out.println("RCON is running on port: " + rconPort);
-
         rcon = RconClient.open(HOST, rconPort, rconPassword);
+
+
+        bot = new GenericContainer<>("szelagi/mc-test-player")
+                .withCreateContainerCmdModifier(cmd -> cmd.withName("sessionapi-test-client"))
+                .withEnv("SERVER_HOST", "mc-server")
+                .withEnv("SERVER_PORT", "25565")
+                .withEnv("USERNAME", "TesterBot")
+                .withEnv("MC_VERSION", "1.21.1")
+                .withNetwork(sharedNetwork);
+
+        bot.start();
+
+        Awaitility.await()
+                .pollInterval(Duration.ofMillis(100))
+                .until(() -> {
+                    String logs = server.getLogs();
+                    return logs.contains("joined the game");
+                });
     }
 
     @AfterAll
     public static void teardown() {
         if (rcon != null) rcon.close();
+        if (bot != null) bot.stop();
         if (server != null) server.stop();
     }
 
     @Test
+    public void hasPlayer() {
+        var res = rcon.sendCommand("list");
+        System.out.println(res.toString());
+        var pattern = Pattern.compile(" 0 ");
+        var matches = pattern.matcher(res).find();
+        Assertions.assertFalse(matches);
+    }
+
+    @Test
     public void hasPlugins() {
-        var query = rcon.sendCommand("pl");
-        if (!query.contains("SessionAPI"))
+        var res = rcon.sendCommand("pl");
+        if (!res.contains("SessionAPI"))
             throw new IllegalStateException("SessionAPI not present");
 
-        if (!query.contains("FastAsyncWorldEdit"))
+        if (!res.contains("FastAsyncWorldEdit"))
             throw new IllegalStateException("FastAsyncWorldEdit not present");
     }
 
@@ -145,9 +185,9 @@ public class CoreIT {
 
     private void internalTest(@NotNull String name, @Nullable List<String> args) throws IntegrationTestFailed {
         var query = "session-integration-test " + name + (args == null || args.isEmpty() ? "" : String.join(" ", args));
-        var response = rcon.sendCommand(query);
-        if (!response.contains(IntegrationTestCommand.SUCCESS)) {
-            throw new IntegrationTestFailed(response);
+        var res = rcon.sendCommand(query);
+        if (!res.contains(IntegrationTestCommand.SUCCESS)) {
+            throw new IntegrationTestFailed(res);
         } else {
             System.out.println("Successfully executed: " + name);
         }
